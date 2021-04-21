2d6e65db0eb909de661713f00e98f78631ff30c9
pedrobratti <pedro.bratti@dinocloudconsulting.com>
2021-04-21 11:19:54 -0300

    set up tf files

--- main.tf
+++ main.tf
@@ -1,3 +1,19 @@
+terraform {
+  required_version = "~> 0.14"
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 3.0"
+    }
+  }
+  backend "s3" {
+    bucket  = "terraform-access-key-rotation"
+    key     = "infra_rotation_keys.tfstate"
+    region  = "us-east-1"
+    encrypt = true
+    profile = "default"
+  }
+}
 provider "aws" {
   region  = "us-east-1"
   profile = "default"
@@ -57,7 +73,7 @@ PATTERN
 resource "aws_cloudwatch_event_target" "sns" {
   rule      = aws_cloudwatch_event_rule.compliance_change.name
   target_id = "SNS"
-  arn       = "${aws_sns_topic.sns_event_target.arn}"
+  arn       = aws_sns_topic.sns_lambda.arn
 }
 
 # Create SNS CloudWatch Event Target
@@ -85,42 +101,42 @@ EOF
 }
 
 # Create Lambda fuction python 3.8
-resource "aws_iam_role" "iam_for_lambda" {
-  name = "AWSRoleForLambdaAccessKeysRotationPoc"
+# resource "aws_iam_role" "iam_for_lambda" {
+#   name = "AWSRoleForLambdaAccessKeysRotationPoc"
 
-  assume_role_policy = <<EOF
-{
-    "Version": "2012-10-17",
-    "Statement": [
-        {
-            "Effect": "Allow",
-            "Action": "*",
-            "Resource": "*"
-        }
-    ]
-}
-EOF
-}
+#   assume_role_policy = <<EOF
+# {
+#     "Version": "2012-10-17",
+#     "Statement": [
+#         {
+#             "Effect": "Allow",
+#             "Action": "*",
+#             "Resource": "*"
+#         }
+#     ]
+# }
+# EOF
+# }
 
-resource "aws_lambda_function" "iam_lambda" {
-  filename      = "iam_lambda.zip"
-  function_name = "lambda_function_name"
-  role          = aws_iam_role.iam_for_lambda.arn
-  handler       = "exports.test"
+# resource "aws_lambda_function" "iam_lambda" {
+#   filename      = "iam_lambda.zip"
+#   function_name = "lambda_function_name"
+#   role          = aws_iam_role.iam_for_lambda.arn
+#   handler       = "exports.test"
 
-  # The filebase64sha256() function is available in Terraform 0.11.12 and later
-  # For Terraform 0.11.11 and earlier, use the base64sha256() function and the file() function:
-  # source_code_hash = "${base64sha256(file("lambda_function_payload.zip"))}"
-  source_code_hash = filebase64sha256("lambda_function_payload.zip")
+#   # The filebase64sha256() function is available in Terraform 0.11.12 and later
+#   # For Terraform 0.11.11 and earlier, use the base64sha256() function and the file() function:
+#   # source_code_hash = "${base64sha256(file("lambda_function_payload.zip"))}"
+#   source_code_hash = filebase64sha256("lambda_function_payload.zip")
 
-  runtime = "nodejs12.x"
+#   runtime = "nodejs12.x"
 
-  environment {
-    variables = {
-      foo = "bar"
-    }
-  }
-}
+#   environment {
+#     variables = {
+#       foo = "bar"
+#     }
+#   }
+# }
 output "rule" {
-  value = aws_config_config_rule.rule.id
+  value = aws_config_config_rule.rule.arn
 }
